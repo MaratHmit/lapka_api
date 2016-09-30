7102a913a0fc4af5f873becce5719776e5246f79
MaratHmit <hmm1979@mail.ru>
2016-09-30 18:31:58 +0500

    Категории новостей

--- vendor/SE/Shop/NewsCategory.php
+++ vendor/SE/Shop/NewsCategory.php
@@ -4,7 +4,7 @@ namespace SE\Shop;
 
 class NewsCategory extends Base
 {
-    protected $tableName = "news_category";
+    protected $tableName = "news_group";
     protected $sortBy = "sort";
     protected $sortOrder = "asc";
 
